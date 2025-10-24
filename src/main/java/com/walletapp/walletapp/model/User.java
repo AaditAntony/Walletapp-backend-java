3678cfced7c4f2616b91f95166e83e345858a4a4
Aadit Antony <aaditantony1112@gmail.com>
2025-10-24 23:23:16 +0530

    done the first part

---
+++ src/main/java/com/walletapp/walletapp/model/User.java
@@ -0,0 +1,46 @@
+package com.walletapp.walletapp.model;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+    @PositiveOrZero(message = "Balance cannot be negative")
+
+    private double balance;
+    public User(){}
+    public User(String name,double balance){
+        this.name= name;
+        this.balance= balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
